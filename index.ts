8f963b50b7dc5bb54f914719ea65ea4477420172
ringo360 <105296365+ringo360@users.noreply.github.com>
2024-01-07 19:50:37 +0900

    hmm didn't work

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('8264')
+import * as Misskey from 'misskey-js';
+import config from './config.json';
+
+console.log('Connecting to ' + config.host + '...')
+const stream = new Misskey.Stream(`https://${config.host}`, { token: config.token });
+const mainChannel = stream.useChannel('main');
+mainChannel.on('notification', notification => {
+	console.log('notification received', notification);
+});
